d55a8f610bfb631486e56658d7e133d95ea9ec4d
Frank Torres <frank.torres@retabet.es>
2024-03-04 10:58:28 +0100

    Metrics collected

--- Program.cs
+++ Program.cs
@@ -4,85 +4,128 @@ namespace CaughtInLoop;
 
 class Program
 {
-    private const int LOOPS = 5001;
-    private const int PARALLELISM = 50;
+	private const int LOOPS = 5001;
+	private const int PARALLELISM = 50;
 
-    static void Main(string[] args)
-    {
-        WithoutHandlingInnerExceptions(NotProducingExeptions, new Action<string>((m) => {}));
+	static void Main(string[] args)
+	{
+		// dumb execution - not collecting logs - init calls to collect better metrics for the rest of the calls
+		NotHandlingInnerExceptions(NotProducingExeptions, new Action<string>((m) => { }));
 
-        Console.WriteLine("WithoutHandlingInnerExceptions(NotProducingExeptions)");
-        MeasureExecution(() => WithoutHandlingInnerExceptions(NotProducingExeptions, Console.WriteLine));
-        Console.WriteLine("-------------------------------------------------");
+		// actual calls to collect metrics from
+		Console.WriteLine("NotAsyncHandlingInnerExceptions (ProducingExeptions)");
+		MeasureExecution(() => NotAsyncHandlingInnerExceptions(ProducingExeptions, Console.WriteLine));
+		Console.WriteLine("-------------------------------------------------\n");
 
-        Console.WriteLine("WithoutHandlingInnerExceptions(ProducingExeptions)");
-        MeasureExecution(() => WithoutHandlingInnerExceptions(ProducingExeptions, Console.WriteLine));
-        Console.WriteLine("-------------------------------------------------");
+		Console.WriteLine("WithoutHandlingInnerExceptions (NotProducingExeptions)");
+		MeasureExecution(() => NotHandlingInnerExceptions(NotProducingExeptions, Console.WriteLine));
+		Console.WriteLine("-------------------------------------------------\n");
 
-        Console.WriteLine("HandlingInnerExceptions(NotProducingExeptions)");
-        MeasureExecution(() => HandlingInnerExceptions(NotProducingExeptions, Console.WriteLine));
-        Console.WriteLine("-------------------------------------------------");
+		Console.WriteLine("WithoutHandlingInnerExceptions (ProducingExeptions)");
+		MeasureExecution(() => NotHandlingInnerExceptions(ProducingExeptions, Console.WriteLine));
+		Console.WriteLine("-------------------------------------------------\n");
 
-        Console.WriteLine("HandlingInnerExceptions(ProducingExeptions)");
-        MeasureExecution(() => HandlingInnerExceptions(ProducingExeptions, Console.WriteLine));
-    }
+		Console.WriteLine("HandlingInnerExceptions (NotProducingExeptions)");
+		MeasureExecution(() => HandlingInnerExceptions(NotProducingExeptions, Console.WriteLine));
+		Console.WriteLine("-------------------------------------------------\n");
 
-    private static void ProducingExeptions(int loopIndex)
-    {
-        if (loopIndex%1000 == 0) throw new Exception($"Bad behavior on {loopIndex}");
-    }
+		Console.WriteLine("HandlingInnerExceptions (ProducingExeptions)");
+		MeasureExecution(() => HandlingInnerExceptions(ProducingExeptions, Console.WriteLine));
+	}
 
-    private static void NotProducingExeptions(int loopIndex)
-    {
-        // do nothing
-    }
+	private static void MeasureExecution(Action action)
+	{
+		var sw = Stopwatch.StartNew();
+		action();
+		sw.Stop();
+		WaitForAllLogsToBeWritten();
+		Console.WriteLine($"Execution took: {sw.Elapsed}");
+	}
 
-    private static async void HandlingInnerExceptions(Action<int> Operation, Action<string> log)
-    {
-        var totalProcessed = 0;
-        await Parallel.ForEachAsync(Enumerable.Range(1, LOOPS),
-            new ParallelOptions() { MaxDegreeOfParallelism = PARALLELISM },
-            async (i, _) =>
-            {
-                try
-                {
-                    log($"Item: {i} ");
-                    Interlocked.Increment(ref totalProcessed);
-                    Operation(i);
-                }
-                catch (System.Exception ex)
-                {
-                    log($"Exception caught in-loop: {ex.Message}");
-                }
-            });
-            
-        log($"\nTotal processed: {totalProcessed}");
-    }
+	private static void ProducingExeptions(int loopIndex)
+	{
+		if (loopIndex % 200 == 0) throw new Exception($"Bad behavior on {loopIndex}");
+	}
 
+	private static void NotProducingExeptions(int loopIndex)
+	{
+		// do nothing
+	}
 
-    private static async void WithoutHandlingInnerExceptions(Action<int> Operation, Action<string> log)
-    {
-        var totalProcessed = 0;
-        await Parallel.ForEachAsync(Enumerable.Range(1, LOOPS),
-            new ParallelOptions() { MaxDegreeOfParallelism = PARALLELISM },
-            async (i, _) =>
-            {
-                log($"Item: {i} ");
-                Interlocked.Increment(ref totalProcessed);
-                Operation(i);
-            });
-            
-        log($"\nTotal processed: {totalProcessed}");
-    }
+	private static async void NotHandlingInnerExceptions(Action<int> Operation, Action<string> log)
+	{
+		var totalProcessed = 0;
+		try
+		{
+			await Parallel.ForEachAsync(Enumerable.Range(1, LOOPS),
+			new ParallelOptions() { MaxDegreeOfParallelism = PARALLELISM },
+			async (i, _) =>
+			{
+				await Task.Run(() => log($"Item: {i} "));
+				Interlocked.Increment(ref totalProcessed);
+				Operation(i);
+			});
 
-    private static void MeasureExecution(Action action)
-    {
-        var sw = Stopwatch.StartNew();
-        try { action(); }
-        catch {
-            Console.WriteLine($"Exception scalated outside {action.Method.Name}");
-        }
-        sw.Stop();
-        Console.WriteLine($"Execution took: {sw.Elapsed}");
-    }
+		}
+		catch
+		{
+			Console.WriteLine($"Exception scalated outside.");
+		}
+		log($"\nTotal processed: {totalProcessed}");
+	}
+
+	private static async void HandlingInnerExceptions(Action<int> Operation, Action<string> log)
+	{
+		var totalProcessed = 0;
+		try
+		{
+			await Parallel.ForEachAsync(Enumerable.Range(1, LOOPS),
+			new ParallelOptions() { MaxDegreeOfParallelism = PARALLELISM },
+			async (i, _) =>
+			{
+				try
+				{
+					await Task.Run(() => log($"Item: {i} "));
+					Interlocked.Increment(ref totalProcessed);
+					Operation(i);
+				}
+				catch (System.Exception ex)
+				{
+					log($"Exception caught in-loop: {ex.Message}");
+				}
+			});
+		}
+		catch
+		{
+			Console.WriteLine($"Exception scalated outside.");
+		}
+		log($"\nTotal processed: {totalProcessed}");
+	}
+
+	private static void NotAsyncHandlingInnerExceptions(Action<int> Operation, Action<string> log)
+	{
+		var totalProcessed = 0;
+		Parallel.ForEach(Enumerable.Range(1, LOOPS),
+			new ParallelOptions() { MaxDegreeOfParallelism = PARALLELISM },
+			(i, _) =>
+			{
+				try
+				{
+					log($"Item: {i} ");
+					Interlocked.Increment(ref totalProcessed);
+					Operation(i);
+				}
+				catch (System.Exception ex)
+				{
+					log($"Exception caught in-loop: {ex.Message}");
+				}
+			});
+
+		log($"\nTotal processed: {totalProcessed}");
+	}
+
+	private static void WaitForAllLogsToBeWritten()
+	{
+		Task.Delay(1000).Wait();
+	}
 }
